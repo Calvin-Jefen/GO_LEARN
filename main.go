465bd0ac32f8a73552b4982eb4bcadf9ca18a47e
Calvin-Jefen <calvinkumambong@gmail.com>
2023-03-22 17:21:02 +0700

    Trying Number, Boolean and String

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main() {
+	/*Hello World Trial*/
+	// fmt.Println("Hello World")
+
+	/*Number Trial*/
+	// fmt.Println("Satu : ", 1)
+	// fmt.Println("Dua  :", 2)
+	// fmt.Println("Tiga Koma Lima :", 3.5)
+
+	/* BOOLEAN TRIAL */
+	// fmt.Println("Benar = ", true)
+	// fmt.Println("Salah = ", false)
+
+	/* STRING TRIAL */
+	fmt.Println("Calvin")
+	fmt.Println("Calvin Jefen")
+	fmt.Println("Calvin Jefen Kumambong")
+
+	fmt.Println(len("Calvin"))
+	fmt.Println("Calvin Jefen"[0])
+	fmt.Println("Calvin Jefen Kumambong"[1])
+
+}
