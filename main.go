a4edf6aaea60c688d92db9b55932d6a26437f27e
Calvin-Jefen <calvinkumambong@gmail.com>
2023-03-23 07:02:16 +0700

    Belajar Pagi, Kamis 23 Maret 2023

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ func main() {
 	/*Hello World Trial*/
 	// fmt.Println("Hello World")
 
-	/*Number Trial*/
+	/* NUMBER TRIAL */
 	// fmt.Println("Satu : ", 1)
 	// fmt.Println("Dua  :", 2)
 	// fmt.Println("Tiga Koma Lima :", 3.5)
@@ -16,12 +16,40 @@ func main() {
 	// fmt.Println("Salah = ", false)
 
 	/* STRING TRIAL */
-	fmt.Println("Calvin")
-	fmt.Println("Calvin Jefen")
-	fmt.Println("Calvin Jefen Kumambong")
+	// fmt.Println("Calvin")
+	// fmt.Println("Calvin Jefen")
+	// fmt.Println("Calvin Jefen Kumambong")
 
-	fmt.Println(len("Calvin"))
-	fmt.Println("Calvin Jefen"[0])
-	fmt.Println("Calvin Jefen Kumambong"[1])
+	// fmt.Println(len("Calvin"))
+	// fmt.Println("Calvin Jefen"[0])
+	// fmt.Println("Calvin Jefen Kumambong"[1])
+
+	/* VARIABLE TRIAL */
+	var name string
+	name = "Calvin Jefen"
+	fmt.Println(name)
+
+	name = "Calvin Kumambong"
+	fmt.Println(name)
+
+	var myname = "Calvin Jefen1"
+	fmt.Println(myname)
+
+	myname = "Calvin Kumambong1"
+	fmt.Println(myname)
+
+	Name := "Calvin Jefen2"
+	fmt.Println(Name)
+
+	Name = "Calvin Kumambong2"
+	fmt.Println(Name)
+
+	var (
+		firstname = "Calvin Jefen"
+		lastname  = "Kumambong"
+	)
+
+	fmt.Println(firstname)
+	fmt.Println(lastname)
 
 }
