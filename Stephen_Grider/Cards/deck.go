358676a23a5e43647c9fe5cc2ac4fc26843b3cea
Calvin-Jefen <calvinkumambong@gmail.com>
2023-04-22 18:58:14 +0700

    Added toString conversion function and saveToFile functionality in deck.go

--- Stephen_Grider/Cards/deck.go
+++ Stephen_Grider/Cards/deck.go
@@ -1,8 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"strings"
+)
 
-// Create a new type of 'deck'
 // which is a slice of strings
 type deck []string
 
@@ -28,4 +31,12 @@ func (d deck) print() {
 
 func deal(d deck, handsize int) (deck, deck) {
 	return d[:handsize], d[handsize:]
+}
+
+func (d deck) toString() string {
+	return strings.Join([]string(d), ",")
+}
+
+func (d deck) saveToFile(filename string) error {
+	return os.WriteFile(filename, []byte(d.toString()), 0666)
 }
--- Stephen_Grider/Cards/main.go
+++ Stephen_Grider/Cards/main.go
@@ -1,14 +1,19 @@
 package main
 
-func main() {
-	cards := newDeck()
-	// cards = append(cards, "Six of Spades")
+import "fmt"
 
-	// cards.print()
-	hand, remainingCards := deal(cards, 5)
-	hand.print()
-	// fmt.Println("-----------")
-	remainingCards.print()
+func main() {
+	// cards := newDeck()
+	// // cards = append(cards, "Six of Spades")
+
+	// // cards.print()
+	// hand, remainingCards := deal(cards, 5)
+	// hand.print()
+	// // fmt.Println("-----------")
+	// remainingCards.print()
+
+	cards := newDeck()
+	fmt.Println(cards.toString())
 }
 
 // func newcard() string {
